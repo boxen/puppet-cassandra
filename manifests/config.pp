6558a51dd04b81670073c4450182979ae7f2acd9
Will Farrington <wfarr@github.com>
2013-02-04 20:41:12 -0800

    Move cassandra to non-default ports, and add some more sauce to the launchctl script

--- manifests/config.pp
+++ manifests/config.pp
@@ -5,5 +5,11 @@ class cassandra::config {
   $datadir    = "${boxen::config::datadir}/cassandra"
   $executable = "${boxen::config::home}/homebrew/bin/cassandra"
   $logdir     = "${boxen::config::logdir}/cassandra"
+  $logfile    = "${logdir}/cassandra.log"
   $cluster    = 'github-dev'
+
+
+  $storage_port = 17000
+  $ssl_storage_port = 17001
+  $rpc_port = 19160
 }
